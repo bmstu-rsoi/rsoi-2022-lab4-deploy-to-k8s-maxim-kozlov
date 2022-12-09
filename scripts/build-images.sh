3aa85b3d0cb94f96eaec020965731000bc52ab11
Maxim <mad-maxkozlov@mail.ru>
2022-12-09 13:47:37 +0300

    #1 aâ

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -2,6 +2,6 @@ version=1.0
 
 cd ./src
 docker buildx build --push -t maxkozlov/rsoi-api-gateway:$version    --platform=linux/amd64 -f ./FlightBooking.Gateway/Dockerfile .
-docker buildx build --push -t maxkozlov/rsoi-ticket-service:$version --platform=linux/amd64 -f ./FlightBooking.BonusService/Dockerfile .
+docker buildx build --push -t maxkozlov/rsoi-bonus-service:$version --platform=linux/amd64 -f ./FlightBooking.BonusService/Dockerfile .
 docker buildx build --push -t maxkozlov/rsoi-flight-service:$version --platform=linux/amd64 -f ./FlightBooking.FlightService/Dockerfile .
-docker buildx build --push -t maxkozlov/rsoi-bonus-service:$version  --platform=linux/amd64 -f ./FlightBooking.TicketService/Dockerfile .
+docker buildx build --push -t maxkozlov/rsoi-ticket-service:$version  --platform=linux/amd64 -f ./FlightBooking.TicketService/Dockerfile .
